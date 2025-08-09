71c33720bbea4421ef6fc15327021d3389797db1
Nodar Okroshiashvili <n.okroshiashvili@gmail.com>
2025-08-10 01:16:52 +0400

    Code quality improvements for tests (#21)
    * Apply code quality improvements and refactoring to tests.
    
    * Linting and formatting changes
    
    * Fix typo in test file name

--- tests/test_index_creation_async.py
+++ tests/test_index_creation_async.py
@@ -1,50 +1,33 @@
+"""Test async index creation and registration in collections."""
+
+from typing import Any
+
 import pytest
-import pytest_asyncio
-from typing import List
-from pydantic import BaseModel, Field
-from pydongo import as_collection
+
+from pydongo.drivers.mock import MockAsyncMongoDBDriver
 from pydongo.expressions.index import (
-    IndexSortOrder,
-    IndexType,
     CollationStrength,
     IndexExpression,
+    IndexSortOrder,
+    IndexType,
 )
-from pydongo.drivers.mock import MockAsyncMongoDBDriver
+from pydongo.workers.collection import CollectionWorker
 
 
-class DemoModel(BaseModel):
-    name: str
-    email: str
-    bio: str
-    hash_id: str
-    latlon: List[float] = Field(default_factory=lambda: [0.0, 0.0])
-    point: dict = Field(
-        default_factory=lambda: {"type": "Point", "coordinates": [0.0, 0.0]}
-    )
-
-
-def _assert_index_registered(collection, driver, expected_count):
-    assert len(collection._indexes) == expected_count, (
-        "Index should be registered in collection"
-    )
-    assert len(driver.indexes[collection.collection_name]) == expected_count, (
-        "Driver should contain the index"
-    )
-    assert all(
-        isinstance(ix, IndexExpression) for index in collection._indexes for ix in index
-    )
-
-
-@pytest_asyncio.fixture
-async def setup_async_collection():
-    driver = MockAsyncMongoDBDriver()
-    await driver.connect()
-    collection = as_collection(DemoModel, driver)
-    return collection, driver
+def _assert_index_registered(
+    collection: CollectionWorker[Any], driver: MockAsyncMongoDBDriver, expected_count: int
+) -> None:
+    """Assert that the index is registered in the collection and driver."""
+    assert len(collection._indexes) == expected_count, "Index should be registered in collection"
+    assert len(driver.indexes[collection.collection_name]) == expected_count, "Driver should contain the index"
+    assert all(isinstance(ix, IndexExpression) for index in collection._indexes for ix in index)
 
 
 @pytest.mark.asyncio
-async def test_single_key_index_async(setup_async_collection):
+async def test_single_key_index_async(
+    setup_async_collection: tuple[CollectionWorker[Any], MockAsyncMongoDBDriver],
+) -> None:
+    """Test single key index creation and registration."""
     collection, driver = setup_async_collection
     index = collection.name.as_index().use_unique().build_index()
 
@@ -56,18 +39,13 @@ async def test_single_key_index_async(setup_async_collection):
 
 
 @pytest.mark.asyncio
-async def test_compound_index_async(setup_async_collection):
+async def test_compound_index_async(
+    setup_async_collection: tuple[CollectionWorker[Any], MockAsyncMongoDBDriver],
+) -> None:
+    """Test compound index creation and registration."""
     collection, driver = setup_async_collection
-    index1 = (
-        collection.name.as_index()
-        .use_sort_order(IndexSortOrder.ASCENDING)
-        .build_index()
-    )
-    index2 = (
-        collection.email.as_index()
-        .use_sort_order(IndexSortOrder.DESCENDING)
-        .build_index()
-    )
+    index1 = collection.name.as_index().use_sort_order(IndexSortOrder.ASCENDING).build_index()
+    index2 = collection.email.as_index().use_sort_order(IndexSortOrder.DESCENDING).build_index()
 
     assert isinstance(index1, IndexExpression)
     assert isinstance(index2, IndexExpression)
@@ -82,12 +60,13 @@ async def test_compound_index_async(setup_async_collection):
 
 
 @pytest.mark.asyncio
-async def test_text_and_hash_index_async(setup_async_collection):
+async def test_text_and_hash_index_async(
+    setup_async_collection: tuple[CollectionWorker[Any], MockAsyncMongoDBDriver],
+) -> None:
+    """Test text and hash index creation and registration."""
     collection, driver = setup_async_collection
     text_index = collection.bio.as_index().use_index_type(IndexType.TEXT).build_index()
-    hash_index = (
-        collection.hash_id.as_index().use_index_type(IndexType.HASHED).build_index()
-    )
+    hash_index = collection.hash_id.as_index().use_index_type(IndexType.HASHED).build_index()
 
     assert isinstance(text_index, IndexExpression)
     assert isinstance(hash_index, IndexExpression)
@@ -98,26 +77,19 @@ async def test_text_and_hash_index_async(setup_async_collection):
 
     _assert_index_registered(collection, driver, 2)
 
-    index_types = [
-        ix[0].index_type for ix in driver.indexes[collection.collection_name]
-    ]
+    index_types = [ix[0].index_type for ix in driver.indexes[collection.collection_name]]
     assert IndexType.TEXT in index_types
     assert IndexType.HASHED in index_types
 
 
 @pytest.mark.asyncio
-async def test_geo_index_types_async(setup_async_collection):
+async def test_geo_index_types_async(
+    setup_async_collection: tuple[CollectionWorker[Any], MockAsyncMongoDBDriver],
+) -> None:
+    """Test creation of 2D and 2D Sphere geo indexes."""
     collection, driver = setup_async_collection
-    index_2d = (
-        collection.latlon.as_index()
-        .use_index_type(IndexType.TWO_DIMENSIONAL)
-        .build_index()
-    )
-    index_2dsphere = (
-        collection.point.as_index()
-        .use_index_type(IndexType.TWO_DIMENSIONAL_SPHERE)
-        .build_index()
-    )
+    index_2d = collection.latlon.as_index().use_index_type(IndexType.TWO_DIMENSIONAL).build_index()
+    index_2dsphere = collection.point.as_index().use_index_type(IndexType.TWO_DIMENSIONAL_SPHERE).build_index()
 
     assert isinstance(index_2d, IndexExpression)
     assert isinstance(index_2dsphere, IndexExpression)
@@ -128,15 +100,16 @@ async def test_geo_index_types_async(setup_async_collection):
 
     _assert_index_registered(collection, driver, 2)
 
-    index_types = [
-        ix[0].index_type for ix in driver.indexes[collection.collection_name]
-    ]
+    index_types = [ix[0].index_type for ix in driver.indexes[collection.collection_name]]
     assert IndexType.TWO_DIMENSIONAL in index_types
     assert IndexType.TWO_DIMENSIONAL_SPHERE in index_types
 
 
 @pytest.mark.asyncio
-async def test_index_with_special_kwargs_async(setup_async_collection):
+async def test_index_with_special_kwargs_async(
+    setup_async_collection: tuple[CollectionWorker[Any], MockAsyncMongoDBDriver],
+) -> None:
+    """Test index creation with special kwargs like unique, sparse, ttl, and collation."""
     collection, driver = setup_async_collection
     index = (
         collection.email.as_index()
